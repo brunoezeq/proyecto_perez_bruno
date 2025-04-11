737a1d8939c24e1b6a7ae8445bbbfb46ffb7f513
Bruno <ezeqperez53@gmail.com>
2025-04-10 21:52:29 -0300

    version 0.0.3

--- app/Views/front/contacto.php
+++ app/Views/front/contacto.php
@@ -4,39 +4,35 @@
   <p>En XPRESSO S.A, nuestra prioridad sos vos. Estamos aquí para ti en cada paso del camino, listos para responder tus preguntas, atender tus necesidades y proporcionarte la mejor experiencia posible. 
   No dudes en ponerte en contacto con nosotros utilizando la información que se encuentra a continuación. Estamos aquí para servirte y asegurarnos de que obtengas todo lo que necesitas.</p>
 
-  <div class="form-contacto">
-    <div class="form-de-contacto">
-      <form action="">
-        <p>
-          <label>Nombre completo</label>
-          <input type="text" name="nombre completo">
-        </p>
-        <p>
-          <label>Correo electrónico</label>
-          <input type="email" name="correo electrónico">
-        </p>
-        <p>
-          <label>Teléfono</label>
-          <input type="tel" name="teléfono">
-        </p>
-        <p>
-          <label>Mensaje</label>
-          <textarea name="mensaje" rows="3"></textarea>
-        </p>
-        <p>
-          <button type="submit">ENVIAR</button>
-        </p>
-      </form>
+  <div class="container my-5">
+  <h1 class="text-center mb-4">Contacto</h1>
+  <form class="row g-3">
+    <div class="col-md-6">
+      <label for="nombre" class="form-label">Nombre</label>
+      <input type="text" class="form-control" id="nombre" placeholder="Tu nombre">
     </div>
-    <div class="info-de-contacto">
+    <div class="col-md-6">
+      <label for="email" class="form-label">Correo</label>
+      <input type="email" class="form-control" id="email" placeholder="tu@correo.com">
+    </div>
+    <div class="col-12">
+      <label for="mensaje" class="form-label">Mensaje</label>
+      <textarea class="form-control" id="mensaje" rows="4" placeholder="Escribí tu mensaje aquí"></textarea>
+    </div>
+    <div class="col-12 text-center">
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+  </form>
+
+  <div class="info-de-contacto">
         <h4>Más información</h4>
         <ul>
-          <li><img src="http://localhost/proyecto_perez_bruno/assets/img/direccion.png"> Güemes 165, H3500 Resistencia, Chaco</li>
-          <li><img src="http://localhost/proyecto_perez_bruno/assets/img/telefono.png"> 3624-091245</li>
-          <li><img src="http://localhost/proyecto_perez_bruno/assets/img/telefono.png"> 3624-091246</li>
-          <li><img src="http://localhost/proyecto_perez_bruno/assets/img/correo.png"> xpresso-contact@gmail.com</li>
+          <li><img src="assets/img/direccion.png"> Güemes 165, H3500 Resistencia, Chaco</li>
+          <li><img src="assets/img/telefono.png"> 3624-091245</li>
+          <li><img src="assets/img/telefono.png"> 3624-091246</li>
+          <li><img src="assets/img/correo.png"> xpresso-contact@gmail.com</li>
         </ul>
     </div>
-  </div>
+</div>
 
 </div>
--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -4,34 +4,98 @@
 <title><?= $titulo ?> </title>  
 <meta name="viewport" content="width=device-width, initialscale=1,  
 shrink-to-fit=no"> 
-<link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 <link href="assets/css/estilo.css" rel="stylesheet" > 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
 </head>  
-<body>  
 
-  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="assets/img/CARRUSEL_1.png" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+<body>
+  <img class="img-fluid" src="assets/img/bienvenida.png" alt="cartel-bienvenida">   
+  
+  <div class= "prod-destacados"> 
+    
+    <h2>Productos Destacados</h2>
+
+    <div class="container text-center">
+    <div class="row">
+
+    <div class="col">
+      <div class="card" style="width: 18rem;" >
+      <img src="assets/img/bolsa_cafe_colombia.png" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+      <img src="assets/img/bolsa_cafe_kenya.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+      <img src="assets/img/bolsa_cafe_guatemala.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
   </div>
 
-<script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner container-fluid">
+    <div class="carousel-item active">
+      <img src="assets/img/CARRUSEL_1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/CARRUSEL_2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/CARRUSEL_3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+  <div class="container text-center info-cafe">
+  <h2>Descubrí la mejor preparación para vos</h2>
+  <div class="row">
+    <div class="col-lg-4 col-md-12 col-sm-12">
+      <img src="assets/img/cafe_latte.jpeg" alt="">
+      <p>El café late es la mejor ...</p>
+    </div>
+    <div class="col-lg-4 col-md-12 col-sm-12">
+    <img src="assets/img/cafe_expreso.jpeg" alt="">
+    <p>El café espreso es la mejor ...</p>
+    </div>
+    <div class="col-lg-4 col-md-12 col-sm-12">
+    <img src="assets/img/cafe_cortado.jpeg" alt="">
+    <p>El café cortado es la mejor ...</p>
+    </div>
+  </div>
+</div>
+
+
+</div>
 </body>  
 </html>
+
+
 
